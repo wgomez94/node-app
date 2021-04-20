f04915b752a6a01203ab53fc668b72c9cdcc7581
wgomez@bananacode.co <wgomez@bananacode.co>
2021-04-20 14:36:47 -0600

    /

--- test/text.js
+++ test/text.js
@@ -1,8 +1,8 @@
-var request = require(‘supertest’);
-var app = require(‘../index.js’);
-describe(‘GET /’, function() {
- it(‘respond with hello world’, function(done) {
+var request = require('supertest');
+var app = require('../index.js');
+describe('GET /', function() {
+ it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get(‘/’).expect(‘hello world’, done);
+ request(app).get('/').expect('hello world', done);
  });
 });
